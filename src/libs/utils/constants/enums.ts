e4985cdf3e46d04526bcf8f080c0bb362d236c00
pujanjani27 <pujan.shivinfotech@gmail.com>
2026-02-10 12:20:09 +0530

    add enums, interfaces, messages files in libs/utils/constansts

---
+++ src/libs/utils/constants/enums.ts
@@ -0,0 +1,13 @@
+export enum StatusType {
+  SUCCESS = 'Success',
+  ERROR = 'Error',
+}
+
+export enum ApiTagType {
+  AUTH = 'Auth',
+}
+
+export enum UserRoles {
+  ADMIN = 'admin',
+  USER = 'user',
+}
