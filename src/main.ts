ff2b5bee7212b9947dff6080649276bfe65d3242
pujanjani27 <pujan.shivinfotech@gmail.com>
2026-02-10 12:20:50 +0530

    setup exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './libs/helpers/exception-filter/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const httpAdapterHost: HttpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionFilter(httpAdapterHost));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
