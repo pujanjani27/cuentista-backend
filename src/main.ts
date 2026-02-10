429b4d8fb3de056d1e1dab50d3e037881dc077ef
pujanjani27 <pujan.shivinfotech@gmail.com>
2026-02-10 17:41:47 +0530

    feat: add swagger setup

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './libs/helpers/exception-filter/exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cuentista')
+    .setDescription('Cuentista API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
