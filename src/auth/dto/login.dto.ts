9f1213c8f92cee96aee7d984fd767d0cb30cbbe6
pujanjani27 <pujan.shivinfotech@gmail.com>
2026-02-11 17:43:47 +0530

    docs: change ApiProperty example in login dto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,12 +2,12 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
 
 export class LoginDto {
-  @ApiProperty({ example: 'user@example.com' })
+  @ApiProperty({ example: 'admin@gmail.com' })
   @IsNotEmpty({ message: 'Email is required' })
   @IsEmail()
   email: string;
 
-  @ApiProperty({ example: 'Password123!' })
+  @ApiProperty({ example: 'Admin@1234' })
   @IsNotEmpty({ message: 'Password is required' })
   @IsStrongPassword()
   password: string;
